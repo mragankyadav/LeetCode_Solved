9097e5ecaa5852648cbd19276ea97a52126bb814
mragank <mragank@tamu.edu>
2016-11-27 22:58:32 -0600

    436. Find Right Interval

---
+++ Find Right Interval.py
@@ -0,0 +1,15 @@
+# Definition for an interval.
+# class Interval(object):
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class Solution(object):
+    def findRightInterval(self, intervals):
+        l = sorted((e.start, i) for i, e in enumerate(intervals))
+        res = []
+        for e in intervals:
+            r = bisect.bisect_left(l, (e.end,))
+            res.append(l[r][1] if r < len(l) else -1)
+        return res
+            
