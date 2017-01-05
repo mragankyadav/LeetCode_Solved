5ec0fcbe1532a18ce72f4ed3b17d0b2ff411d672
mragank <mragank@tamu.edu>
2017-01-05 01:10:32 -0600

    Improved

--- 216. Combination Sum III.py
+++ 216. Combination Sum III.py
@@ -1,3 +1,4 @@
+import copy
 class Solution(object):
     def combinationSum3(self, k, n):
         """
@@ -5,16 +6,18 @@ class Solution(object):
         :type n: int
         :rtype: List[List[int]]
         """
-        return self.helper(1,n+1,k,[],[],n)
+        return self.helper(1,10,k,[],[],n)
         
     def helper(self,start,end,k,path,ans,n):
         s= sum(path)
         if len(path)==k or s>n:
-            if s==n and max(path)<=9:
-                ans.append(path)
+            if s==n:
+                ans.append(copy.copy(path))
         else:
             for i in range(start,end):
-                ans=self.helper(i+1,end,k,path+[i],ans,n)
+                path.append(i)
+                ans=self.helper(i+1,end,k,path,ans,n)
+                del path[-1]
         return ans
             
             
